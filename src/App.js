703259ccc09b9f404127a94739b5732cf0f6bd58
mflRevan <aimmanlp@gmail.com>
2023-07-09 19:46:24 +0200

    added chat widget (WIP)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
+import {
+	Widget,
+	addResponseMessage,
+	addUserMessage,
+	toggleWidget,
+} from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
 import Projects from "./pages/projects";
@@ -20,8 +28,20 @@ function App() {
 		}
 	}, []);
 
+	const handleNewUserMessage = (newMessage) => {
+		console.log(`New message incoming! ${newMessage}`);
+		// Now send the message throught the backend API
+		addUserMessage(newMessage);
+		addResponseMessage("Shut");
+	};
+
 	return (
 		<div className="App">
+			<Widget
+				handleNewUserMessage={handleNewUserMessage}
+				title="Aiman"
+				subtitle="Online"
+			/>
 			<Routes>
 				<Route path="/" element={<Homepage />} />
 				<Route path="/about" element={<About />} />
